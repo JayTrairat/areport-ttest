492662a3cc1cb00a2399f88dc8c3449f3e096d9a
JayTrairat <codeoutpost@gmail.com>
2018-04-13 17:03:30 +0700

    refactor(): bring back worked version

--- main.py
+++ main.py
@@ -12,31 +12,23 @@ def main():
         original_contents = [float(content.strip()) for content in original_contents]
         original_contents = np.array(original_contents)
 
-    average_x1 = round(contents.mean(), 4)
-    average_x2 = round(original_contents.mean(), 4)
+    average_x1 = contents.mean()
+    average_x2 = original_contents.mean()
 
-    sd_sd1 = round(np.std(contents), 4)
-    sd_sd2 = round(np.std(original_contents), 4)
-
-    v_1 = round(sd_sd1 ** 2, 4)
-    v_2 = round(sd_sd2 ** 2, 4)
+    sd_sd1 = np.std(contents)
+    sd_sd2 = np.std(original_contents)
 
     n_n1 = len(contents)
     n_n2 = len(original_contents)
 
-    divider = round(np.sqrt((v_1 + v_2)/100), 4)
-
-
-    dividey = round((average_x1 - average_x2), 4)
+    z_value = (average_x1 - average_x2)/np.sqrt((sd_sd1 ** 2 / n_n1) + (sd_sd2 ** 2 / n_n2))
     print('average x1 :: ' + str(average_x1))
     print('average x2 :: ' + str(average_x2))
     print('standard deviation 1 :: ' + str(sd_sd1))
     print('standard deviation 2 :: ' + str(sd_sd2))
-    print('variance 1 :: ' + str(v_1))
-    print('variance 2 :: ' + str(v_2))
-    # print('members 1 :: ' + str(n_n1))
-    # print('members 2 :: ' + str(n_n2))
-    print('z-value :: ' + str(round((dividey/divider), 2)))
+    print('members 1 :: ' + str(n_n1))
+    print('members 2 :: ' + str(n_n2))
+    print('z-value :: ' + str(z_value))
 
 if __name__ == '__main__':
     main()
