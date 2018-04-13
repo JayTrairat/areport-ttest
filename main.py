e809cd1416a84ff6ba9748e188125d516c78ecc3
JayTrairat <codeoutpost@gmail.com>
2018-04-13 15:30:37 +0700

    refactor(): use 30 instead of 100

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import numpy as np
 from scipy import stats
 
 def main():
-    with open('files/generate.txt', 'r', encoding='utf8') as source:
+    with open('files/generate_for_ttest.txt', 'r', encoding='utf8') as source:
         contents = (source.readlines())
         contents = [float(content.strip()) for content in contents]
         contents = np.array(contents)
 
-    with open('files/original.txt', 'r', encoding='utf8') as source:
+    with open('files/original_for_ttest.txt', 'r', encoding='utf8') as source:
         original_contents = (source.readlines())
         original_contents = [float(content.strip()) for content in original_contents]
         original_contents = np.array(original_contents)
@@ -24,14 +24,21 @@ def main():
     n_n1 = len(contents)
     n_n2 = len(original_contents)
 
-    z_value = (average_x1 - average_x2)/np.sqrt((sd_sd1 ** 2 / n_n1) + (sd_sd2 ** 2 / n_n2))
+    t_value = (average_x1 - average_x2)/np.sqrt((sd_sd1 ** 2 / n_n1) + (sd_sd2 ** 2 / n_n2))
     print('average x1 :: ' + str(average_x1))
     print('average x2 :: ' + str(average_x2))
     print('standard deviation 1 :: ' + str(sd_sd1))
     print('standard deviation 2 :: ' + str(sd_sd2))
     print('members 1 :: ' + str(n_n1))
     print('members 2 :: ' + str(n_n2))
-    print('z-value :: ' + str(z_value))
+    print('t-value :: ' + str(t_value))
+
+    ddof = (n_n1 + n_n2) - 2
+    ## Cross Checking with the internal scipy function
+    t2, p2 = stats.ttest_ind(original_contents, contents)
+    print("t = " + str(t2))
+    print("p = " + str(2*p2))
+
 
 if __name__ == '__main__':
     main()
