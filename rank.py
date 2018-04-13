8f2aed60d0dfbe1d3ee22a93ee0265e5170642d0
JayTrairat <codeoutpost@gmail.com>
2018-04-13 14:25:19 +0700

    feat(): ranked function

---
+++ rank.py
@@ -0,0 +1,17 @@
+def main():
+    with open('files/generate.txt', 'r', encoding='utf8') as sources:
+        contents = sources.readlines()
+        contents = [content.strip() for content in contents]
+        contents.sort(reverse=True)
+        with open('files/generate_for_ttest.txt', 'w', encoding='utf8') as writer:
+            writer.write('\n'.join(contents[:100]))
+
+    with open('files/original.txt', 'r', encoding='utf8') as sources:
+        contents = sources.readlines()
+        contents = [content.strip() for content in contents]
+        contents.sort(reverse=True)
+        with open('files/original_for_ttest.txt', 'w', encoding='utf8') as writer:
+            writer.write('\n'.join(contents[:100]))
+
+if __name__ == '__main__':
+    main()
